4365d137ead5c397205d77c0960e85fe97d73a8d
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-17 11:47:56 +0300

    copy

---
+++ NAPLEX/Classes/Misc/GlobalDefinitions.swift
@@ -0,0 +1,25 @@
+//
+//  GlobalDefinitions.swift
+//  Nursing
+//
+//  Created by Andrey Chernyshev on 16.01.2021.
+//
+
+final class GlobalDefinitions {
+    static let domainUrl = "https://exams.korrekted.com"
+    static let apiKey = "qnEIZYcLdKsO0fbU"
+
+    static let sdkDomainUrl = "https://analytics.korrekted.com"
+    static let sdkApiKey = "hd9AtCxh9P5x6XQw"
+
+    static let applicationTag = "EXAM_NAPLEX"
+
+    static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
+    static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+
+    static let appleAppID = "1567717921"
+
+    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
+    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
+    static let contactUsUrl = "https://quantresear.ch/contact.html"
+}
