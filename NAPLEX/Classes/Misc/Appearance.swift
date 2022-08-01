077b7d9cd6fe7685fb32a078f1a250be4926c8af
akonst17 <akonst17@gmail.com>
2022-08-01 15:48:26 +0300

    обновления Nursing; build 13

--- NAPLEX/Classes/Misc/Appearance.swift
+++ NAPLEX/Classes/Misc/Appearance.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 struct Appearance {
-    static let backgroundColor = UIColor(integralRed: 255, green: 252, blue: 254)
-    
     static let mainColor = UIColor(integralRed: 149, green: 77, blue: 185)
     static let mainColorAlpha = UIColor(integralRed: 149, green: 77, blue: 185, alpha: 0.3)
-    
-    static let takeFreeTestColor = UIColor(integralRed: 231, green: 60, blue: 63)
-    
-    static let progress1Color = UIColor(integralRed: 149, green: 77, blue: 185)
-    static let progress2Color = UIColor(integralRed: 231, green: 60, blue: 63)
-    static let progress3Color = UIColor(integralRed: 39, green: 170, blue: 255)
-    
+    static let secondColor = UIColor(integralRed: 39, green: 170, blue: 255)
+    static let thirdColor = UIColor(integralRed: 231, green: 60, blue: 63)
     static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
     static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
     static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
+    static let backgroundColor = UIColor(integralRed: 247, green: 245, blue: 255)
+    static let greyColor = UIColor(integralRed: 75, green: 81, blue: 102)
+    static let blackColor = UIColor(integralRed: 19, green: 24, blue: 42)
 }
