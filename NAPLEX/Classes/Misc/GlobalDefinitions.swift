b1cc01994b3a7b7e9ba86aca402575f23460e041
akonst17 <akonst17@gmail.com>
2022-09-20 10:56:36 +0300

    интеграция sdk 2.0; новые урлы; build 12

--- NAPLEX/Classes/Misc/GlobalDefinitions.swift
+++ NAPLEX/Classes/Misc/GlobalDefinitions.swift
@@ -6,21 +6,24 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.korrekted.com"
+    static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "qnEIZYcLdKsO0fbU"
-
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "hd9AtCxh9P5x6XQw"
 
     static let applicationTag = "EXAM_NAPLEX"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "hd9AtCxh9P5x6XQw"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "tQkrxrO6ygmP9K3j"
 
     static let appStoreUrl = "https://apps.apple.com/app/naplex-exam-test-prep-app/id1567717921"
     static let appleAppID = "1567717921"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
